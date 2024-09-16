88e194e5bbcf0b4ddcdf8c050b7076e7a4a75b52
jamesjiangxing <jiang_xing@hotmail.com>
2024-09-16 13:48:27 +0800

    add directory

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const app = express();
 const PORT = 4000;
 
 app.get('/home', (req, res) => {
-  res.status(200).json('Welcome, your app is working well');
+  res.status(200).json('Welcome, your app is working well on home');
 });
+
+app.get('/', (req, res) => {
+    res.status(200).json('Welcome, your app is working well');
+  });
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
